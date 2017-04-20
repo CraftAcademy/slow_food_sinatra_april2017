bd1233b29acae77a9c4722ac257ca4b111e498dc
Benji <benjamincomley@gmail.com>
2017-04-20 14:00:11 +0200

    updating test script

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,9 +1,9 @@
 #You can implement step definitions for undefined steps with these snippets:
 
-Given(/^I am on the "([^"]*)"$/) do
+Given(/^I am on the homepage$/) do
   visit '/'
 end
 
-Then(/^I should be able to see "([^"]*)"$/) do |arg1|
+Then(/^I should be able to see welcome message$/) do
   expect(page).to have_content('Welcome to Slow Food Restaurant')
 end
