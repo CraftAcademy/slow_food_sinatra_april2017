85fe78d3e27779a95a3018df68439492ba72866f
mmollerstrom <mmollerstrom@icloud.com>
2017-04-21 12:31:03 +0200

    resolves conflict, tuned up view, test success

--- features/step_definitions/view_dishes_steps.rb
+++ features/step_definitions/view_dishes_steps.rb
@@ -1,6 +1,5 @@
 
 Given(/^there is a dish called "([^"]*)" on "([^"]*)"$/) do |dishName, menuName|
-  #binding.pry
   menu = Menu.create(name: menuName)
   menu.dishes.create(name: dishName, menu_id: menu.id)
   menu.save
@@ -12,8 +11,4 @@ end
 
 Then(/^I should be able to see "([^"]*)"$/) do |content|
   expect(page).to have_content content
-end
-
-Then(/^show me the page$/) do
-  save_and_open_page
 end
