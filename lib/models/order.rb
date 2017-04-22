56b9fb1cc342ea8b04c9e34c6a32e2d04d3bb66c
Raoul DIFFOUO <diraulo@gmail.com>
2017-04-22 17:25:16 +0200

    test green: can add a single item to the order

--- lib/models/order.rb
+++ lib/models/order.rb
@@ -4,27 +4,28 @@ class Order
 
   property :id, Serial, key: true
 
-  has n, :OrderItems
+  has n, :order_items
 end
 
 
-
-class Buy
-
-  attr_accessor :cart, :order, :in_item_cart
-
-  def initialize(menu_name, dish_name)
-    @menu = Menu.first(name: menu_name)
-    @dish = Dish.first(name: dish_name)
-    @order = Order.create()
-  end
-
-  def add_to_cart
-    binding.pry
-    @cart = OrderItem.create(:dish_id => @dish.id, :dish_menu_id => @dish.menu_id, :order_id => @order.id)
-    if OrderItem.first(:dish_id => @dish.id)
-      @in_item_cart = @dish
-    end
-  end
-
-end
+# class Buy
+#
+#   attr_accessor :cart, :order, :in_item_cart
+#
+#   def initialize(menu_name, dish_name)
+#     @menu = Menu.first(name: menu_name)
+#     @dish = Dish.first(name: dish_name)
+#     @order = Order.create()
+#     @cart = []
+#   end
+#
+#   def add_to_cart
+#     binding.pry
+#     @cart << OrderItem.create(dish_id: @dish.id, dish_menu_id: @dish.menu_id, order_id: @order.id)
+#
+#     # if OrderItem.first(dish_id: @dish.id)
+#     #   @in_item_cart = @dish
+#     # end
+#   end
+#
+# end
